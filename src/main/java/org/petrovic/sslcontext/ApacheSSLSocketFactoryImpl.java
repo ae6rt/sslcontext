f5f1f6deaad894cb96d6a40daadfc104839ea914
Mark S. Petrovic <mspetrovic@gmail.com>
2012-03-22 20:58:51 -0700

    add Commons HTTP client Protocol and socket factory as examples

---
+++ src/main/java/org/petrovic/sslcontext/ApacheSSLSocketFactoryImpl.java
@@ -0,0 +1,50 @@
+package org.petrovic.sslcontext;
+
+import org.apache.commons.httpclient.ConnectTimeoutException;
+import org.apache.commons.httpclient.params.HttpConnectionParams;
+import org.apache.commons.httpclient.protocol.SecureProtocolSocketFactory;
+
+import javax.net.SocketFactory;
+import javax.net.ssl.SSLContext;
+import java.io.IOException;
+import java.net.*;
+
+public class ApacheSSLSocketFactoryImpl implements SecureProtocolSocketFactory {
+
+    private SSLContext context;
+
+    public ApacheSSLSocketFactoryImpl(SSLContext context) {
+        this.context = context;
+    }
+
+    public Socket createSocket(Socket socket, String s, int i, boolean b) throws IOException, UnknownHostException {
+        return context.getSocketFactory().createSocket(socket, s, i, b);
+    }
+
+    public Socket createSocket(String s, int i, InetAddress inetAddress, int i1) throws IOException, UnknownHostException {
+        return context.getSocketFactory().createSocket(s, i, inetAddress, i1);
+    }
+
+    public Socket createSocket(String host, int port, InetAddress inetAddress, int i1, HttpConnectionParams params)
+            throws IOException, UnknownHostException, ConnectTimeoutException {
+        if (params == null) {
+            throw new IllegalArgumentException("Parameters may not be null");
+        }
+        int timeout = params.getConnectionTimeout();
+        SocketFactory socketfactory = context.getSocketFactory();
+        if (timeout == 0) {
+            return socketfactory.createSocket(host, port, inetAddress, i1);
+        } else {
+            Socket socket = socketfactory.createSocket();
+            SocketAddress localaddr = new InetSocketAddress(inetAddress, i1);
+            SocketAddress remoteaddr = new InetSocketAddress(host, port);
+            socket.bind(localaddr);
+            socket.connect(remoteaddr, timeout);
+            return socket;
+        }
+    }
+
+    public Socket createSocket(String s, int i) throws IOException, UnknownHostException {
+        return context.getSocketFactory().createSocket(s, i);
+    }
+}
--- src/test/java/org/petrovic/sslcontext/CustomSSLContextTest.java
+++ src/test/java/org/petrovic/sslcontext/CustomSSLContextTest.java
@@ -1,5 +1,6 @@
 package org.petrovic.sslcontext;
 
+import org.apache.commons.httpclient.protocol.Protocol;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -14,8 +15,7 @@ import java.security.*;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 /**
  * @author petrovic -- 3/22/12 11:19 AM
@@ -70,6 +70,19 @@ public class CustomSSLContextTest {
         // I can imagine cases where it would want the underlying SSLSocketFactory, too.  YMMV
         SSLSocketFactory socketFactory = context.getSocketFactory();
         assertNotNull(socketFactory);
+    }
+
+
+    @Test
+    public void testCommonsClient() throws IOException, NoSuchAlgorithmException, KeyStoreException, CertificateException {
+        CustomSSLContextBag contextBag = new CustomSSLContextBag();
+        contextBag.setTrustStore(keyStoreFromFile(new File("truststore.jks"), "changeit")); // contains the new VeriSign cert
+        contextBag.setKeyManager(null);  // don't need a keymanager bc server does not require client auth
+        contextBag.init();
+        Protocol.registerProtocol("https", new Protocol("https", new ApacheSSLSocketFactoryImpl(contextBag.getContext()), 443));
+
+        // doesn't test much, but it should run without exception
+        assertTrue(true);
     }
 
     private KeyStore keyStoreFromFile(File keyStoreFile, String storepass) throws KeyStoreException, IOException, NoSuchAlgorithmException, CertificateException {
